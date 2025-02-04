075b82270f463233a7958792d9e97a3c864f42aa
Seth <rseth@gtlpc101.georgiatech-metz.fr>
2025-02-04 11:25:10 +0100

    Initial odom implementation

--- ros2_ws/src/rover_driver_base/rover_driver_base/rover_kinematics.py
+++ ros2_ws/src/rover_driver_base/rover_driver_base/rover_kinematics.py
@@ -98,13 +98,26 @@ class RoverKinematics:
 
     def prepare_inversion_matrix(self,drive_cfg):
         # TODO: Build pseudo inverse of W using the notation from the class. The matrix size below is wrong.
-        iW = np.asmatrix(np.zeros((3,2)))
+        temp = []
+        for k in drive_cfg.keys():
+            temp.append([1, 0, -drive_cfg[k].y])
+            temp.append([0, 1, drive_cfg[k].x])
+        W = np.array(temp)
+        iW = np.linalg.pinv(W)
         return iW
 
     def prepare_displacement_matrix(self, motor_state_t1, motor_state_t2, drive_cfg):
         # then compute odometry using least square
         # TODO: Build S using the notation from the class. The matrix size below is wrong.
-        S = np.asmatrix(np.zeros((2,1)))
+        temp = []
+        for k in drive_cfg.keys():
+            disp = motor_state_t2.drive[k] - motor_state_t1.drive[k]
+            corrected_disp = atan2(sin(disp), cos(disp))
+            s = corrected_disp * drive_cfg[k].radius
+            beta = (motor_state_t2.steering[k] + motor_state_t1.steering[k]) / 2.0
+            temp.append(s*cos(beta))
+            temp.append(s*sin(beta))
+        S = np.array(temp)
         return S
 
     def compute_displacement(self, motor_state, drive_cfg):
@@ -120,7 +133,7 @@ class RoverKinematics:
         # TODO: Then compute the displacement matrix S based on the current motor_state, and the previous one stored in self.motor_state
         S = self.prepare_displacement_matrix(self.motor_state,motor_state,drive_cfg)
         # Finally the displacement in the local frame is the product
-        dX = iW * S 
+        dX = iW @ S 
         # And we backup the current motor state
         self.motor_state.copy(motor_state)
         return dX
@@ -129,9 +142,9 @@ class RoverKinematics:
         # First compute the local displacement in the robot frame
         dX = self.compute_displacement(motor_state,drive_cfg)
         # TODO: Now integrate the local displacement in the global frame
-        self.X[0,0] += 0.0
-        self.X[1,0] += 0.0
-        self.X[2,0] += 0.0
+        self.X[0,0] += dX[0]
+        self.X[1,0] += dX[1]
+        self.X[2,0] += dX[2]
         return self.X
 
 
